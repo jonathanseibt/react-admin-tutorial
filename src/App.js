2ad645646600485504e5a9fb9f7733a01143bfe5
Jonathan Seibt <56838120+jonathanseibt@users.noreply.github.com>
2020-11-19 11:48:33 -0300

    feat: add posts resource

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Admin, Resource } from "react-admin";
+import { Admin, Resource, ListGuesser } from "react-admin";
 import { UserList } from "./users";
 import jsonServerProvider from "ra-data-json-server";
 
@@ -7,6 +7,7 @@ const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={ListGuesser} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
