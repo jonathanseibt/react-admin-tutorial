36f92a89038d94a5002db58a99b33593e68a299f
Jonathan Seibt <56838120+jonathanseibt@users.noreply.github.com>
2020-11-19 12:18:42 -0300

    feat: replace edit guesser with custom edit on posts resource

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Admin, Resource, EditGuesser } from "react-admin";
-import { PostList } from "./posts";
+import { Admin, Resource } from "react-admin";
+import { PostList, PostEdit } from "./posts";
 import { UserList } from "./users";
 import jsonServerProvider from "ra-data-json-server";
 
@@ -8,7 +8,7 @@ const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={EditGuesser} />
+    <Resource name="posts" list={PostList} edit={PostEdit} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
